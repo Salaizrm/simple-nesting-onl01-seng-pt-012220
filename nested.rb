2a7345701f4bea716a570eb5e2ad69d1a42f8c5e
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-01 00:47:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].first 
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,6 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash << [:yukihiro_matsumoto]
 
     
 end
